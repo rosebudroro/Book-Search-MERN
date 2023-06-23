0b01acd34aa7ecf528d6f91128e261493743da35
Roxy Osorio <roxy.osorio@moo.com>
2023-06-23 19:44:44 -0400

    server.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 function App() {
   return (
